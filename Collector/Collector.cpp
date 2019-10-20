d07830781eb8608f1ada45837a8918688bef3d2c
YUSUF ARSLAN POLAT <l5213034@std.yildiz.edu.tr>
2019-10-20 21:17:55 +0300

    communication with usermode.

--- Collector/Collector.cpp
+++ Collector/Collector.cpp
@@ -6,192 +6,329 @@
 
 // prototypes
 
-void CollectorUnload(_In_ PDRIVER_OBJECT DriverObject);
-NTSTATUS CollectorCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp);
-NTSTATUS CollectorDeviceControl(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp);
-LONG GetProcessList();
+extern "C" {
+	NTSTATUS
+		DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath);
+	void CollectorUnload(_In_ PDRIVER_OBJECT DriverObject);
+	NTSTATUS CollectorCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp);
+	NTSTATUS CollectorDeviceControl(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp);
+	LONG GetProcessList();
+	VOID
+		PrintIrpInfo(
+			PIRP Irp
+		);
+	VOID
+		PrintChars(
+			_In_reads_(CountChars) PCHAR BufferAddress,
+			_In_ size_t CountChars
+		);
 
-fnZwQuerySystemInformation ZwQuerySystemInformation = NULL;
-PPROCESS_HEAD_LIST g_processheadlist = NULL;
-// DriverEntry
+	fnZwQuerySystemInformation ZwQuerySystemInformation = NULL;
+	PPROCESS_HEAD_LIST g_processheadlist = NULL;
+#ifdef ALLOC_PRAGMA
+#pragma alloc_text( INIT, DriverEntry )
+#pragma alloc_text( PAGE, CollectorUnload)
+#pragma alloc_text( PAGE, CollectorDeviceControl)
+#pragma alloc_text( PAGE, GetProcessList)
+#pragma alloc_text( PAGE, PrintIrpInfo)
+#pragma alloc_text( PAGE, PrintChars)
+#endif // ALLOC_PRAGMA
+	// DriverEntry
+	extern "C" NTSTATUS
+		DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
+		UNREFERENCED_PARAMETER(RegistryPath);
 
-extern "C" NTSTATUS
-DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
-	UNREFERENCED_PARAMETER(RegistryPath);
+		KdPrint(("Collector DriverEntry started\n"));
 
-	KdPrint(("Collector DriverEntry started\n"));
+		DriverObject->DriverUnload = CollectorUnload;
 
-	DriverObject->DriverUnload = CollectorUnload;
+		DriverObject->MajorFunction[IRP_MJ_CREATE] = CollectorCreateClose;
+		DriverObject->MajorFunction[IRP_MJ_CLOSE] = CollectorCreateClose;
+		DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = CollectorDeviceControl;
 
-	DriverObject->MajorFunction[IRP_MJ_CREATE] = CollectorCreateClose;
-	DriverObject->MajorFunction[IRP_MJ_CLOSE] = CollectorCreateClose;
-	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = CollectorDeviceControl;
-
-	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\Collector");
-	//RtlInitUnicodeString(&devName, L"\\Device\\ThreadBoost");
-	PDEVICE_OBJECT DeviceObject;
-	NTSTATUS status = IoCreateDevice(DriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, FALSE, &DeviceObject);
-	if (!NT_SUCCESS(status)) {
-		KdPrint(("Failed to create device (0x%08X)\n", status));
-		return status;
-	}
-
-	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Collector");
-	status = IoCreateSymbolicLink(&symLink, &devName);
-	if (!NT_SUCCESS(status)) {
-		KdPrint(("Failed to create symbolic link (0x%08X)\n", status));
-		IoDeleteDevice(DeviceObject);
-		return status;
-	}
-
-	KdPrint(("Collector DriverEntry completed successfully\n"));
-
-	return STATUS_SUCCESS;
-}
-
-void CollectorUnload(_In_ PDRIVER_OBJECT DriverObject) {
-	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Collector");
-	// delete symbolic link
-	IoDeleteSymbolicLink(&symLink);
-
-	// delete device object
-	IoDeleteDevice(DriverObject->DeviceObject);
-
-	KdPrint(("Collector unloaded\n"));
-}
-
-_Use_decl_annotations_
-NTSTATUS CollectorCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
-	UNREFERENCED_PARAMETER(DeviceObject);
-
-	Irp->IoStatus.Status = STATUS_SUCCESS;
-	Irp->IoStatus.Information = 0;
-	IoCompleteRequest(Irp, IO_NO_INCREMENT);
-	return STATUS_SUCCESS;
-}
-
-_Use_decl_annotations_
-NTSTATUS CollectorDeviceControl(PDEVICE_OBJECT, PIRP Irp) {
-	// get our IO_STACK_LOCATION
-	auto stack = IoGetCurrentIrpStackLocation(Irp);
-	auto status = STATUS_SUCCESS;
-
-	switch (stack->Parameters.DeviceIoControl.IoControlCode) {
-	case IOCTL_EVIDENCE_COLLECTOR_GET_PROCESSLIST:
-	{
-		// do the work
-		if (stack->Parameters.DeviceIoControl.InputBufferLength < sizeof(Config)) {
-			status = STATUS_BUFFER_TOO_SMALL;
-			break;
-		}
-		if (stack->Parameters.DeviceIoControl.OutputBufferLength < 0x10000) {
-			status = STATUS_BUFFER_TOO_SMALL;
-			break;
+		UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\Collector");
+		//RtlInitUnicodeString(&devName, L"\\Device\\ThreadBoost");
+		PDEVICE_OBJECT DeviceObject;
+		NTSTATUS status = IoCreateDevice(DriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, FALSE, &DeviceObject);
+		if (!NT_SUCCESS(status)) {
+			KdPrint(("Failed to create device (0x%08X)\n", status));
+			return status;
 		}
 
-		auto data = (Config*)stack->Parameters.DeviceIoControl.Type3InputBuffer;
-		if (data == nullptr) {
-			status = STATUS_INVALID_PARAMETER;
-			break;
+		UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Collector");
+		status = IoCreateSymbolicLink(&symLink, &devName);
+		if (!NT_SUCCESS(status)) {
+			KdPrint(("Failed to create symbolic link (0x%08X)\n", status));
+			IoDeleteDevice(DeviceObject);
+			return status;
 		}
-		GetProcessList();
-		KdPrint(("GetProcessList executed."));
-		break;
+
+		KdPrint(("Collector DriverEntry completed successfully\n"));
+
+		return STATUS_SUCCESS;
 	}
 
-	default:
-		status = STATUS_INVALID_DEVICE_REQUEST;
-		break;
+	void CollectorUnload(_In_ PDRIVER_OBJECT DriverObject) {
+		PAGED_CODE();
+		UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Collector");
+		// delete symbolic link
+		IoDeleteSymbolicLink(&symLink);
+
+		// delete device object
+		IoDeleteDevice(DriverObject->DeviceObject);
+
+		KdPrint(("Collector unloaded\n"));
 	}
 
-	Irp->IoStatus.Status = status;
-	Irp->IoStatus.Information = 0;
-	IoCompleteRequest(Irp, IO_NO_INCREMENT);
-	return status;
-}
+	_Use_decl_annotations_
+		NTSTATUS CollectorCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+		PAGED_CODE();
+		UNREFERENCED_PARAMETER(DeviceObject);
 
-LONG GetProcessList() {
-	KdPrint(("GetProcessList started."));
-	LONG st;
-	PVOID mem;
-	UNICODE_STRING ustr1;
-	ULONG bytes = 0;
-	PSYSTEM_PROCESS_INFO info = NULL;
-	PPROCESS_BUFFER alloc = NULL;
-	PPROCESS_BUFFER buffer = NULL;
+		Irp->IoStatus.Status = STATUS_SUCCESS;
+		Irp->IoStatus.Information = 0;
+		IoCompleteRequest(Irp, IO_NO_INCREMENT);
+		return STATUS_SUCCESS;
+	}
 
-	RtlSecureZeroMemory(&ustr1, sizeof(ustr1));
-	RtlInitUnicodeString(&ustr1, L"ZwQuerySystemInformation");
+	_Use_decl_annotations_
+		NTSTATUS CollectorDeviceControl(PDEVICE_OBJECT, PIRP Irp) {
+		PIO_STACK_LOCATION  irpSp;// Pointer to current stack location
+		NTSTATUS            ntStatus = STATUS_SUCCESS;// Assume success
+		ULONG               inBufLength; // Input buffer length
+		ULONG               outBufLength; // Output buffer length
+		PCHAR               inBuf, outBuf; // pointer to Input and output buffer
+		PCHAR               data = "This String is from Device Driver !!!";
+		size_t              datalen = strlen(data) + 1;//Length of data including null
+		PAGED_CODE();
 
-	ZwQuerySystemInformation = (fnZwQuerySystemInformation)MmGetSystemRoutineAddress(&ustr1);
-	if (ZwQuerySystemInformation == NULL)
-		return STATUS_UNSUCCESSFUL;
+		irpSp = IoGetCurrentIrpStackLocation(Irp);
+		inBufLength = irpSp->Parameters.DeviceIoControl.InputBufferLength;
+		outBufLength = irpSp->Parameters.DeviceIoControl.OutputBufferLength;
 
-	g_processheadlist = (PPROCESS_HEAD_LIST)ExAllocatePoolWithTag(NonPagedPool, sizeof(PROCESS_HEAD_LIST), DRIVER_TAG);
-	if (g_processheadlist == NULL)
-		return LIST_ENTRY_ZERO_MEMORY;
-
-	g_processheadlist->NumberOfProcess = 0;
-
-	st = ZwQuerySystemInformation(SystemProcessInformation, NULL, 0, &bytes);
-
-	if (st == STATUS_INFO_LENGTH_MISMATCH)
-	{
-		mem = ExAllocatePool(NonPagedPool, bytes);
-
-		if (mem != NULL)
+		if (!inBufLength || !outBufLength)
 		{
-			st = ZwQuerySystemInformation(SystemProcessInformation, mem, bytes, &bytes);
+			ntStatus = STATUS_INVALID_PARAMETER;
+			goto End;
+		}
 
-			if (NT_SUCCESS(st))
+		//
+		// Determine which I/O control code was specified.
+		//
+
+		switch (irpSp->Parameters.DeviceIoControl.IoControlCode)
+		{
+		case IOCTL_EVIDENCE_COLLECTOR_GET_PROCESSLIST:
+
+			//
+			// In this method the I/O manager allocates a buffer large enough to
+			// to accommodate larger of the user input buffer and output buffer,
+			// assigns the address to Irp->AssociatedIrp.SystemBuffer, and
+			// copies the content of the user input buffer into this SystemBuffer
+			//
+
+			DbgPrint("Called IOCTL_SIOCTL_METHOD_BUFFERED\n");
+			PrintIrpInfo(Irp);
+
+			//
+			// Input buffer and output buffer is same in this case, read the
+			// content of the buffer before writing to it
+			//
+
+			inBuf = (PCHAR)Irp->AssociatedIrp.SystemBuffer;
+			outBuf = (PCHAR)Irp->AssociatedIrp.SystemBuffer;
+
+			//
+			// Read the data from the buffer
+			//
+
+			DbgPrint("\tData from User :");
+			//
+			// We are using the following function to print characters instead
+			// DebugPrint with %s format because we string we get may or
+			// may not be null terminated.
+			//
+			PrintChars(inBuf, inBufLength);
+
+			//
+			// Write to the buffer over-writes the input buffer content
+			//
+
+			RtlCopyBytes(outBuf, data, outBufLength);
+
+			DbgPrint("\tData to User : ");
+			PrintChars(outBuf, datalen);
+			GetProcessList();
+			//
+			// Assign the length of the data copied to IoStatus.Information
+			// of the Irp and complete the Irp.
+			//
+
+			Irp->IoStatus.Information = (outBufLength < datalen ? outBufLength : datalen);
+
+			//
+			// When the Irp is completed the content of the SystemBuffer
+			// is copied to the User output buffer and the SystemBuffer is
+			// is freed.
+			//
+			break;
+		default:
+
+			//
+			// The specified I/O control code is unrecognized by this driver.
+			//
+
+			ntStatus = STATUS_INVALID_DEVICE_REQUEST;
+			DbgPrint("ERROR: unrecognized IOCTL %x\n",
+				irpSp->Parameters.DeviceIoControl.IoControlCode);
+			break;
+		}
+
+	End:
+		//
+		// Finish the I/O operation by simply completing the packet and returning
+		// the same status as in the packet itself.
+		//
+
+		Irp->IoStatus.Status = ntStatus;
+
+		IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+		return ntStatus;
+	}
+
+	LONG GetProcessList() {
+		PAGED_CODE();
+		KdPrint(("GetProcessList started."));
+		LONG st;
+		PVOID mem;
+		UNICODE_STRING ustr1;
+		ULONG bytes = 0;
+		PSYSTEM_PROCESS_INFO info = NULL;
+		PPROCESS_BUFFER alloc = NULL;
+		PPROCESS_BUFFER buffer = NULL;
+
+		RtlSecureZeroMemory(&ustr1, sizeof(ustr1));
+		RtlInitUnicodeString(&ustr1, L"ZwQuerySystemInformation");
+
+		ZwQuerySystemInformation = (fnZwQuerySystemInformation)MmGetSystemRoutineAddress(&ustr1);
+		if (ZwQuerySystemInformation == NULL)
+			return STATUS_UNSUCCESSFUL;
+
+		g_processheadlist = (PPROCESS_HEAD_LIST)ExAllocatePoolWithTag(NonPagedPool, sizeof(PROCESS_HEAD_LIST), DRIVER_TAG);
+		if (g_processheadlist == NULL)
+			return LIST_ENTRY_ZERO_MEMORY;
+
+		g_processheadlist->NumberOfProcess = 0;
+
+		st = ZwQuerySystemInformation(SystemProcessInformation, NULL, 0, &bytes);
+
+		if (st == STATUS_INFO_LENGTH_MISMATCH)
+		{
+			mem = ExAllocatePool(NonPagedPool, bytes);
+
+			if (mem != NULL)
 			{
-				info = (PSYSTEM_PROCESS_INFO)mem;
+				st = ZwQuerySystemInformation(SystemProcessInformation, mem, bytes, &bytes);
 
-				if (info && MmIsAddressValid(info))
+				if (NT_SUCCESS(st))
 				{
-
-					InitializeListHead(&g_processheadlist->Entry);
-
-					while (info->NextEntryOffset)
+					info = (PSYSTEM_PROCESS_INFO)mem;
+					if (info && MmIsAddressValid(info))
 					{
-						info = (PSYSTEM_PROCESS_INFO)((PUCHAR)info + info->NextEntryOffset);
+
+						InitializeListHead(&g_processheadlist->Entry);
+
+						while (info->NextEntryOffset)
+						{
+							info = (PSYSTEM_PROCESS_INFO)((PUCHAR)info + info->NextEntryOffset);
 
 
-						buffer = (PPROCESS_BUFFER)ExAllocatePool(NonPagedPool, sizeof(PROCESS_BUFFER));
-						RtlSecureZeroMemory(buffer, sizeof(PROCESS_BUFFER));
+							buffer = (PPROCESS_BUFFER)ExAllocatePool(NonPagedPool, sizeof(PROCESS_BUFFER));
+							RtlSecureZeroMemory(buffer, sizeof(PROCESS_BUFFER));
 
-						buffer->ProcessId = PtrToUlong(info->ProcessId);
+							buffer->ProcessId = PtrToUlong(info->ProcessId);
 
-						buffer->ProcessInherited = PtrToUlong(info->InheritedFromProcessId);
+							buffer->ProcessInherited = PtrToUlong(info->InheritedFromProcessId);
 
-						buffer->NumberOfThreads = info->NumberOfThreads;
+							buffer->NumberOfThreads = info->NumberOfThreads;
 
-						RtlCopyMemory(buffer->ProcessName, info->ImageName.Buffer, info->ImageName.Length);
+							RtlCopyMemory(buffer->ProcessName, info->ImageName.Buffer, info->ImageName.Length);
 
-						InsertHeadList(&g_processheadlist->Entry, &buffer->Entry);
+							InsertHeadList(&g_processheadlist->Entry, &buffer->Entry);
 
-						g_processheadlist->NumberOfProcess++;
+							g_processheadlist->NumberOfProcess++;
 
+						}
 					}
 				}
+				ExFreePool(mem);
 			}
-			ExFreePool(mem);
 		}
+		while (!IsListEmpty(&g_processheadlist->Entry))
+		{
+			alloc = (PPROCESS_BUFFER)RemoveHeadList(&g_processheadlist->Entry);
+
+			DbgPrint("\n%1ws %20lu %20lu %20lu", alloc->ProcessName, alloc->ProcessId, alloc->ProcessInherited, alloc->NumberOfThreads);
+			ExFreePool(alloc);
+		}
+		
+		ExFreePoolWithTag(g_processheadlist, DRIVER_TAG);
+
+		g_processheadlist = NULL;
+
+
+		return STATUS_SUCCESS;
 	}
 
-	while (!IsListEmpty(&g_processheadlist->Entry))
+	VOID
+		PrintIrpInfo(
+			PIRP Irp)
 	{
-		alloc = (PPROCESS_BUFFER)RemoveHeadList(&g_processheadlist->Entry);
+		PIO_STACK_LOCATION  irpSp;
+		irpSp = IoGetCurrentIrpStackLocation(Irp);
 
-		DbgPrint("\n%1ws %20lu %20lu %20lu", alloc->ProcessName, alloc->ProcessId, alloc->ProcessInherited, alloc->NumberOfThreads);
+		PAGED_CODE();
 
-		ExFreePool(alloc);
+		DbgPrint("\tIrp->AssociatedIrp.SystemBuffer = 0x%p\n",
+			Irp->AssociatedIrp.SystemBuffer);
+		DbgPrint("\tIrp->UserBuffer = 0x%p\n", Irp->UserBuffer);
+		DbgPrint("\tirpSp->Parameters.DeviceIoControl.Type3InputBuffer = 0x%p\n",
+			irpSp->Parameters.DeviceIoControl.Type3InputBuffer);
+		DbgPrint("\tirpSp->Parameters.DeviceIoControl.InputBufferLength = %d\n",
+			irpSp->Parameters.DeviceIoControl.InputBufferLength);
+		DbgPrint("\tirpSp->Parameters.DeviceIoControl.OutputBufferLength = %d\n",
+			irpSp->Parameters.DeviceIoControl.OutputBufferLength);
+		return;
 	}
 
-	ExFreePoolWithTag(g_processheadlist, DRIVER_TAG);
+	VOID
+		PrintChars(
+			_In_reads_(CountChars) PCHAR BufferAddress,
+			_In_ size_t CountChars
+		)
+	{
+		PAGED_CODE();
 
-	g_processheadlist = NULL;
+		if (CountChars) {
 
+			while (CountChars--) {
 
-	return STATUS_SUCCESS;
+				if (*BufferAddress > 31
+					&& *BufferAddress != 127) {
+
+					DbgPrint("%c", *BufferAddress);
+
+				}
+				else {
+
+					DbgPrint(".");
+
+				}
+				BufferAddress++;
+			}
+			DbgPrint("\n");
+		}
+		return;
+	}
 }
--- ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -3,6 +3,7 @@
 #include "pch.h"
 #include <Windows.h>
 #include <stdio.h>
+#include <strsafe.h>
 #include "..\Collector\CollectorCommon.h"
 
 
@@ -10,31 +11,64 @@ int Error(const char* message) {
 	printf("%s (error=%d)\n", message, GetLastError());
 	return 1;
 }
-
+char OutputBuffer[100];
+char InputBuffer[100];
 int main(int argc, const char* argv[]) {
-	if (argc < 1) {
-		printf("Usage: Collector");
-		return 0;
-	}
+	//if (argc < 1) {
+	//	printf("Usage: Collector");
+	//	return 0;
+	//}
 
-	HANDLE hDevice = CreateFile(L"\\\\.\\Collector", GENERIC_WRITE, FILE_SHARE_WRITE,
-		nullptr, OPEN_EXISTING, 0, nullptr);
+	BOOL bRc;
+	ULONG bytesReturned;
+	DWORD errNum = 0;
+	HANDLE hDevice = CreateFile(L"\\\\.\\Collector", GENERIC_READ | GENERIC_WRITE, 0,
+		nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
 	if (hDevice == INVALID_HANDLE_VALUE)
 		return Error("Failed to open device");
 
-	Config data;
-	//data.Process = TRUE;
-	char OutputBuffer[0x10000];
+	//Config data;
+	//data.Process = (PCHAR)"Hello from user mode";
+	//char OutputBuffer[0x10000];
+	//memset(OutputBuffer, 0, sizeof(OutputBuffer));
+
+	//
+		// Printing Input & Output buffer pointers and size
+		//
+
+	printf("InputBuffer Pointer = %p, BufLength = %Iu\n", InputBuffer,
+		sizeof(InputBuffer));
+	printf("OutputBuffer Pointer = %p BufLength = %Iu\n", OutputBuffer,
+		sizeof(OutputBuffer));
+	//
+	// Performing METHOD_BUFFERED
+	//
+
+	strcpy_s(InputBuffer, sizeof(InputBuffer),
+		"This String is from User Application; using METHOD_BUFFERED");
+
+	printf("\nCalling DeviceIoControl METHOD_BUFFERED:\n");
+
 	memset(OutputBuffer, 0, sizeof(OutputBuffer));
 
-	DWORD returned;
-	BOOL success = DeviceIoControl(hDevice, IOCTL_EVIDENCE_COLLECTOR_GET_PROCESSLIST, &data, sizeof(data), &OutputBuffer, sizeof(OutputBuffer), &returned, nullptr);
-	if (success) {
-		printf("Evidence Collected!\n");
-		printf("OutBuffer (%d): %s\n", returned, OutputBuffer);
+	bRc = DeviceIoControl(hDevice,
+		(DWORD)IOCTL_EVIDENCE_COLLECTOR_GET_PROCESSLIST,
+		&InputBuffer,
+		(DWORD)strlen(InputBuffer) + 1,
+		&OutputBuffer,
+		sizeof(OutputBuffer),
+		&bytesReturned,
+		NULL
+	);
+
+	if (!bRc)
+	{
+		printf("Error in DeviceIoControl : %d", GetLastError());
+		return GetLastError();
+
 	}
-	else
-		Error("Collecting evidence failed!");
+	printf("    OutBuffer (%d): %s\n", bytesReturned, OutputBuffer);
+
 
 	CloseHandle(hDevice);
 }
